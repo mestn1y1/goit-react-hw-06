ca4f91fccf029022d06ff1d586256122f8cf6d69
Serhii Tyron <mestn1y@hotmail.com>
2024-09-14 12:33:05 +0300

    chenge components Contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,14 @@
 import css from "./Contact.module.css";
 import { AiOutlineUser } from "react-icons/ai";
 import { FiPhone } from "react-icons/fi";
+import { useDispatch } from "react-redux";
+import { deleteContact } from "../../redux/contactsSlice";
 
-export default function Contact({ item, onDelete }) {
+export default function Contact({ item }) {
+  const dispatch = useDispatch();
+  const handleDelete = (contactId) => {
+    dispatch(deleteContact(contactId));
+  };
   return (
     <li className={css.container}>
       <div className={css.containerData}>
@@ -15,7 +21,7 @@ export default function Contact({ item, onDelete }) {
           <span className={css.itemEl}>{item.number}</span>
         </div>
       </div>
-      <button className={css.button} onClick={() => onDelete(item.id)}>
+      <button className={css.button} onClick={() => handleDelete(item.id)}>
         Delete
       </button>
     </li>
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,17 +1,12 @@
-import { useSelector, useDispatch } from "react-redux";
-import { selectContacts, deleteContact } from "../../redux/contactsSlice";
+import { useSelector } from "react-redux";
+import { selectContacts } from "../../redux/contactsSlice";
 import { selectFilter } from "../../redux/filtersSlice";
 import Contact from "../Contact/Contact";
 import css from "./ContactList.module.css";
 
 export default function ContactList() {
-  const dispatch = useDispatch();
   const contacts = useSelector(selectContacts);
   const filter = useSelector(selectFilter);
-
-  const handleDelete = (contactId) => {
-    dispatch(deleteContact(contactId));
-  };
 
   const visibleContacts = contacts.filter((contact) =>
     contact.name.toLowerCase().includes(filter.toLowerCase())
@@ -20,7 +15,7 @@ export default function ContactList() {
   return (
     <ul className={css.container}>
       {visibleContacts.map((contact) => (
-        <Contact key={contact.id} item={contact} onDelete={handleDelete} />
+        <Contact key={contact.id} item={contact} />
       ))}
     </ul>
   );
